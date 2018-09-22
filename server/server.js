a828c52120f5aba99de6ca06f1ce66abb7cd05ff
Andrew Ngo <andrew@Andrews-MacBook-Pro-2.local>
2018-09-22 15:07:31 -0700

    Connects deployed components with proxy server

--- server/server.js
+++ server/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const descriptionService = 'http://localhost:3001'
-const photoPortal = 'http://localhost:3007'
-const bookingWidget = 'http://localhost:3003'
-const reviewsService = 'http://localhost:3004'
+const descriptionService = 'http://bearbnbdescription-env1.b4nkf45hnu.us-west-2.elasticbeanstalk.com'
+const photoPortal = 'http://eastphotos-env.fdvpa3pswm.us-east-1.elasticbeanstalk.com/'
+const bookingWidget = 'http://bearbnbbooking-env.bz7dpdnrpj.us-west-1.elasticbeanstalk.com/'
+const reviewsService = 'http://bearbnbreviews-env.rppvejjmcc.us-east-1.elasticbeanstalk.com/'
 
 app.use('/:id', express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
@@ -16,6 +16,8 @@ app.use(morgan('dev'));
 
 app.get('/photos/:id', proxy(photoPortal));
 app.get('/bookings/:id', proxy(bookingWidget));
+app.get('/bookings/:id/check_in/:date', proxy(bookingWidget));
+app.post('/bookings/:id/check_in/:date/check_out/:date', proxy(bookingWidget));
 app.get('/descriptions/:id', proxy(descriptionService));
 app.get('/reviews/:id', proxy(reviewsService));
 
